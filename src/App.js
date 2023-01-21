8d1509df8b7d8f9976548fadcc8e323c31d99ee1
tmannazy <e.anene@hotmail.com>
2023-01-21 08:44:50 +0100

    Remove unused code, add id to route components

--- src/App.js
+++ src/App.js
@@ -11,20 +11,6 @@ function App() {
   // <div className="App">
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <HeaderComponent />
       <div className="container">
         <BrowserRouter>
@@ -33,14 +19,13 @@ function App() {
             <Route path="/employees" element={<ListUserComponent />} />
             <Route
               path="/add-employee/:id"
-              element={<CreateEmployeeComponent />}
+              element={<CreateEmployeeComponent id={`:id`} />}
             />
             <Route
               path="/view-employee/:id"
               exact
               element={<ViewEmployeeComponent id={`:id`} />}
             />
-            {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
           </Routes>
         </BrowserRouter>
       </div>
